c1bfa87532c8701f5adb817313c16a86ad2906ae
ericembestro <japan569@gmail.com>
2017-12-09 17:09:57 +0800

    [Git Issue #41 & #40]

--- WebContent/script/AdminSearch.js
+++ WebContent/script/AdminSearch.js
@@ -13,7 +13,7 @@ function getCollegeValue(collegeAbbreviate){
 					    break;
 		case "CLA":     return "College of Liberal Arts";
 					    break;
-		case "COS":     return "College of Science";
+		case "COS":     return "College of Studies";
 					    break;
 		case "SOE":     return "School of Economics";
 					    break;

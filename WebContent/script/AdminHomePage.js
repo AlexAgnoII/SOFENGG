4d04ec0151ec24bee959ce7bee258185cf2a0a1e
Harvey Lallave <harvey_lallave@gmail.com>
2017-12-16 00:17:09 +0800

    Repushing of UI Posts

--- WebContent/script/AdminHomePage.js
+++ WebContent/script/AdminHomePage.js
@@ -92,6 +92,10 @@ function loadPosts(){
 	
 	        	// Append html snippet 
 	    	    $(postFeed).append(data);
+	    	    
+	    		$('.postBody').each(function() {
+	    	        $(this).height($(this).prop('scrollHeight'));
+	    	    });
         	}
     	},
         error:function(){

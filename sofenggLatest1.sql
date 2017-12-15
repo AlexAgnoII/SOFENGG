1807bc0872f30e7161480aa4fa86c475f998a20b
AlexAgnoII <3alex23@gmail.com>
2017-12-15 18:04:18 +0800

    Please use this latest DB added.

--- sofenggLatest1.sql
+++ sofenggLatest1.sql
@@ -38,7 +38,7 @@ CREATE TABLE `admin` (
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'$31$16$umRipuJiTbFWGJJcWwGeS4rRjd-cn0Ae9sqzM0pwwTs','vicedeanSA@dlsu.edu.ph');
+INSERT INTO `admin` VALUES (1,'$31$16$bzKU3QOXupmf-WojJPrAC6rnU8SqgWnavsZvc3f5fQQ','vicedeanSA@dlsu.edu.ph');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,6 +67,31 @@ INSERT INTO `college` VALUES (1,'College of Computer Studies'),(2,'College of Bu
 UNLOCK TABLES;
 
 --
+-- Table structure for table `expiration_table`
+--
+
+DROP TABLE IF EXISTS `expiration_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `expiration_table` (
+  `expiringID` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`expiringID`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `expiration_table`
+--
+
+LOCK TABLES `expiration_table` WRITE;
+/*!40000 ALTER TABLE `expiration_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `expiration_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `involvement`
 --
 
@@ -81,7 +106,7 @@ CREATE TABLE `involvement` (
   `acadYear` varchar(4) NOT NULL,
   `internal` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`involvementId`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,7 +115,7 @@ CREATE TABLE `involvement` (
 
 LOCK TABLES `involvement` WRITE;
 /*!40000 ALTER TABLE `involvement` DISABLE KEYS */;
-INSERT INTO `involvement` VALUES (19,'Something',22,'Someone','2017',0),(20,'lol',22,'its working!','2000',0),(21,'lmao',22,'yes it is!','2002',0),(22,'BLAH',22,'Someone','1900',1),(23,'normal',22,'mode','1800',1),(24,'hard',22,'MODAL','2020',1),(25,'im new',22,'im added','1111',0),(26,'fuck my life bruh',22,'XD','2030',1),(27,'Future',22,'Robot','3000',1),(28,'',22,'','1234',1),(29,'',22,'','1234',0),(30,'',22,'','1234',0),(31,'',22,'','1234',0),(32,'Something',23,'Someone','2017',1),(33,'I confess my crush',23,'BestBoi','2018',1),(34,'HELP',23,'ME','2019',1);
+INSERT INTO `involvement` VALUES (19,'Something',22,'Someone','2017',0),(20,'lol',22,'its working!','2000',0),(21,'lmao',22,'yes it is!','2002',0),(22,'BLAH',22,'Someone','1900',1),(23,'normal',22,'mode','1800',1),(24,'hard',22,'MODAL','2020',1),(25,'im new',22,'im added','1111',0),(26,'fuck my life bruh',22,'XD','2030',1),(27,'Future',22,'Robot','3000',1),(28,'',22,'','1234',1),(29,'',22,'','1234',0),(30,'',22,'','1234',0),(31,'',22,'','1234',0),(32,'Something',23,'Someone','2017',1),(33,'I confess my crush',23,'BestBoi','2018',1),(34,'HELP',23,'ME','2019',1),(35,'Something',25,'roi','2017',0),(36,'puta',25,'ka','2017',0),(37,'LSCS',25,'President','2016',0),(38,'LSCS',25,'AVP Publications','2017',1),(39,'LSCS ',25,'VP Publications','2018',1),(40,'qwe',25,'zcz','2919',1),(41,'qwe',25,'zxc','2911',1),(42,'asd',25,'xzc','2921',1),(43,'zxc',25,'zxc','2000',1);
 /*!40000 ALTER TABLE `involvement` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,7 +184,7 @@ CREATE TABLE `relative` (
   `occupation` varchar(45) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   PRIMARY KEY (`relativeId`,`studentDBId`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -168,7 +193,7 @@ CREATE TABLE `relative` (
 
 LOCK TABLES `relative` WRITE;
 /*!40000 ALTER TABLE `relative` DISABLE KEYS */;
-INSERT INTO `relative` VALUES (20,22,'Who','Sibling','You','2000-12-01'),(21,22,'Paula Love','Sibling','Tobias','1997-06-09'),(22,22,'Alex  II','Sibling','Agno','1997-12-03'),(23,22,'Papa bear','Father','Bear','2000-12-01'),(24,22,'GALING KO NAMAN','Mother','Bear','2000-12-01'),(25,22,'Charlie','Sibling','The Unicorn','2000-12-10'),(26,23,'Ate bear','Sibling','Bear','2000-12-01'),(27,23,'asd','Sibling','asd','2000-12-01'),(28,23,'Papa bear','Father','Bear','2000-12-01'),(29,23,'Mama bear','Mother','Bear','2000-12-01');
+INSERT INTO `relative` VALUES (20,22,'Who','Sibling','You','2000-12-01'),(22,22,'Alex  II','Sibling','Agno','1997-12-03'),(23,22,'Papa bear','Father','Bear','2000-12-01'),(24,22,'GALING KO NAMAN','Mother','Bear','2000-12-01'),(25,22,'Charlie','Sibling','The Unicorn','2000-12-10'),(26,23,'Ate bear','Sibling','Bear','2000-12-01'),(27,23,'asd','Sibling','asd','2000-12-01'),(28,23,'Papa bear','Father','Bear','2000-12-01'),(29,23,'Mama bear','Mother','Bear','2000-12-01'),(30,25,'Ate bear','Sibling','Bear','2000-12-01'),(31,25,'meow','Sibling','asdasasd','2000-04-01'),(32,25,'Alex','Father','Agno','2000-12-01'),(33,25,'Jess ','Mother','Gano','2000-12-01'),(34,25,'sasdads','Sibling','asdasd','2000-12-01'),(35,25,'aasdasdadasd','Sibling','asdasd','2000-12-01');
 /*!40000 ALTER TABLE `relative` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -212,7 +237,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (10,11433426,'Alexander II','Hernandez','Agno',1,'BSCS-ST',2015,'09178364730','7024242','alexander_agno@dlsu.edu.ph','$31$16$1sTcicgqELbxi55S8R7sUwhe5QOUPXYe4ZGCZtMC7lA','#100 Sct. Boy bata','1997-12-03','Single','Filipino','Male','Philippines','Batangas',1104,'Quezon Ciity',NULL),(11,1919191,'firstname','middlename','lastname',5,'course',NULL,'','','email@address.com','$31$16$JYPe33BLSSdzWs683L_JPUGV2sJsxsVM9GU29cDSsFQ',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(12,1233123,'firstname','hernanDEZZZ','lastname',1,'bs-cs-st',NULL,'12123','11111','agno@alex.com','$31$16$umRipuJiTbFWGJJcWwGeS4rRjd-cn0Ae9sqzM0pwwTs','meowwthan','1990-01-01','wat','male','family','ezCountry','EzPzProvince',0,'Pochinki',NULL),(13,11111111,'Face','Inverted','Troll',1,'BS-CS-ST',NULL,'11111111','111111','hanzo@genji.com','$31$16$TMrNCOtlU6n_nrzNr0Fzy5GuOuIfxfVhLH-BTszZWPM','IDK','1993-03-03','MALE','WEAB','MALE','IDK COUNTRY','IDK PROVINCE',1111,'IDK CITY',NULL),(14,11433426,'Alexander II','Hernandez','Agno',3,'BS-BULLSHIT',NULL,'','','alex@alex.com','$31$16$h439BVKDJJrNYlHWG2uDNKMkIuRYOHjzxHt1l-3493c',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(15,11,'11','11','11',1,'11',NULL,'','','11@11.com','$31$16$S6hJTmHsUkBq0VfaFZ6ehghdBtuFuGrQLunurdQCF7s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(17,56789543,'testing','testing','testing',7,'testing',NULL,'','','test@testing.com','$31$16$gxo0TWZxs0UZvO72wiqcvP9mPGGRUWYt4ALflFphGlo',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(18,88888888,'eight','eight','eight',4,'eight',NULL,'','','eight@eight.com','$31$16$U4MGPBaeOXzqqaUYPFjrfVnOj6uDRCzysdIbM9sTzSc',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(19,77777777,'firstname','middlename','lastname',6,'coursename',NULL,'','','name@this.com','$31$16$MjcjjkXZ0zFT8YuTJDeacuI68vjkM5FJuSOSXNJCHJs',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(20,10010011,'Alexander II','Hernandez','Agno',1,'Software Technology',NULL,'','','alex@pubg.com','$31$16$jOLGGODXz0nfT2FjbRRN8kcDAQcJ5ATmky8jYxpJbw8',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(22,90881231,'Razer','Elegiggle','Mouse',3,'Software Technology',NULL,'','','gonna@test.com','$31$16$_S8xauM0fHKXdw4sH13_38MfpyLgqhVGiHpDhZpTaq0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(23,11000000,'Alexander II','','Agno',4,'asdadads',NULL,'11233333333','70911111','pubg@pubg.com','$31$16$02RmrEhMXbrN9NHzLUdnzyT6bXZxriZGuqIfhWWBoy4','123123976--a-sda-sda-sda-sd-asd','1990-01-01','SINGLE','filipino','male','asdasasd','HELPasdasdasd',111,'is my City',''),(25,90111223,'Alexander II','','Agno',1,'COMPUTER ENG',NULL,'','','3alex23@gmail.com','$31$16$S9LaxrIF6Fi9QP4cvGSZ6OTQiNEGymqgi-iUwS0yN80',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'');
+INSERT INTO `student` VALUES (10,11433426,'Alexander II','Hernandez','Agno',1,'BSCS-ST',2015,'09178364730','7024242','alexander_agno@dlsu.edu.ph','$31$16$1sTcicgqELbxi55S8R7sUwhe5QOUPXYe4ZGCZtMC7lA','#100 Sct. Boy bata','1997-12-03','Single','Filipino','Male','Philippines','Batangas',1104,'Quezon Ciity',NULL),(11,1919191,'firstname','middlename','lastname',5,'course',NULL,'','','email@address.com','$31$16$JYPe33BLSSdzWs683L_JPUGV2sJsxsVM9GU29cDSsFQ',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(12,1233123,'firstname','hernanDEZZZ','lastname',1,'bs-cs-st',NULL,'12123','11111','agno@alex.com','$31$16$umRipuJiTbFWGJJcWwGeS4rRjd-cn0Ae9sqzM0pwwTs','meowwthan','1990-01-01','wat','male','family','ezCountry','EzPzProvince',0,'Pochinki',NULL),(13,11111111,'Face','Inverted','Troll',1,'BS-CS-ST',NULL,'11111111','111111','hanzo@genji.com','$31$16$TMrNCOtlU6n_nrzNr0Fzy5GuOuIfxfVhLH-BTszZWPM','IDK','1993-03-03','MALE','WEAB','MALE','IDK COUNTRY','IDK PROVINCE',1111,'IDK CITY',NULL),(14,11433426,'Alexander II','Hernandez','Agno',3,'BS-BULLSHIT',NULL,'','','alex@alex.com','$31$16$h439BVKDJJrNYlHWG2uDNKMkIuRYOHjzxHt1l-3493c',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(15,11,'11','11','11',1,'11',NULL,'','','11@11.com','$31$16$S6hJTmHsUkBq0VfaFZ6ehghdBtuFuGrQLunurdQCF7s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(17,56789543,'testing','testing','testing',7,'testing',NULL,'','','test@testing.com','$31$16$gxo0TWZxs0UZvO72wiqcvP9mPGGRUWYt4ALflFphGlo',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(18,88888888,'eight','eight','eight',4,'eight',NULL,'','','eight@eight.com','$31$16$U4MGPBaeOXzqqaUYPFjrfVnOj6uDRCzysdIbM9sTzSc',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(19,77777777,'firstname','middlename','lastname',6,'coursename',NULL,'','','name@this.com','$31$16$MjcjjkXZ0zFT8YuTJDeacuI68vjkM5FJuSOSXNJCHJs',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(20,10010011,'Alexander II','Hernandez','Agno',1,'Software Technology',NULL,'','','alex@pubg.com','$31$16$jOLGGODXz0nfT2FjbRRN8kcDAQcJ5ATmky8jYxpJbw8',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(22,90881231,'Razer','Elegiggle','Mouse',3,'Software Technology',NULL,'','','gonna@test.com','$31$16$_S8xauM0fHKXdw4sH13_38MfpyLgqhVGiHpDhZpTaq0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(23,11000000,'Alexander II','','Agno',4,'asdadads',NULL,'11233333333','70911111','pubg@pubg.com','$31$16$02RmrEhMXbrN9NHzLUdnzyT6bXZxriZGuqIfhWWBoy4','123123976--a-sda-sda-sda-sd-asd','1990-01-01','SINGLE','filipino','male','asdasasd','HELPasdasdasd',111,'is my City',''),(25,90111223,'Alexander II','','Agno',1,'COMPUTER ENG',NULL,'','','3alex23@gmail.com','$31$16$vTPeAss-pxaG6TxrfwV7YHk7WDyRPYuknZ41suX70YQ','asadasd','1990-01-01','','','','','',0,'','');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -225,4 +250,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-13  0:16:34
+-- Dump completed on 2017-12-15 18:02:57
